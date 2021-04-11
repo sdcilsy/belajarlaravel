65d660079818ec7322a96e859d66911e5d037341
Muhammad Fakhri Abdillah <fakhriabdillah37@gmail.com>
2021-04-11 11:38:23 +0700

    Belajar Laravel Eps 1 Cilsy x BDV

---
+++ resources/views/cilsy.blade.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+    <title>Did you know about Cilsy?</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <a class="navbar-brand" href="#">Cilsy x BDV</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+            <a class="nav-link" href="/">Home</a>
+            <a class="nav-link active" aria-current="page" href="/cilsy">Did you know about Cilsy?</a>
+            <a class="nav-link" href="/bdv">About BDV</a>
+        </div>
+        </div>
+    </div>
+    </nav>
+    <div class="container">
+    <div class="row">
+    <div class="col-10">
+      <h1 class="mt-3">Cilsy Adalah blablabla</h1>
+    </div>
+    </div>
+    </div>
+  </body>
+</html>
